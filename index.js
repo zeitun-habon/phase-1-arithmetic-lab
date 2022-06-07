edd9690a29bfdeb7e4482c12026bbfa8e09febd5
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-07 10:04:31 +0300

    commit tree c42b61d4cfaa38b858a13e0706d9280456a39248

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 
 // Write your code here
-
-a+b
+let num1=1
+let num2=62
+let multiply = num1*num2
+let random=Math.floor(Math.random() * 10) + 1;
+let num3=29
+let num4=5
+let mod=num3%num4
+let max= Math.max(1, 2, 3, 4, 20);
